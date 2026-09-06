cab004dd16beb043b92dcd1952e077938ff329aa
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2026-09-06 12:55:43 +0900

    filter and sort walk list

---
+++ api/sql/walks/count_by_filters.sql
@@ -0,0 +1,27 @@
+-- $1::bigint - limit
+-- $2::bigint - offset
+-- $3::varchar - name_contains
+-- $4::difficulty - difficulty
+-- $5::uuid - region_id
+--
+SELECT "W"."id",
+       "W"."name",
+       "W"."description",
+       "W"."length_km",
+       "W".image_url,
+       "W"."region_id",
+       "R"."code"      AS "region_code",
+       "R"."name"      AS "region_name",
+       "R"."image_url" AS "region_image_url",
+       "W".difficulty  AS "difficulty: _"
+FROM "walks" AS "W"
+         INNER JOIN
+     "regions" AS "R"
+     ON
+         "W"."region_id" = "R"."id"
+WHERE ($3::varchar IS NULL OR "W"."name" ILIKE '%' || $3::varchar || '%')
+  AND ($4::difficulty IS NULL OR "W"."difficulty" = $4::difficulty)
+  AND ($5::uuid IS NULL OR "W"."region_id" = $5::uuid)
+ORDER BY "W"."id"
+LIMIT $1 OFFSET $2
+;
--- api/src/features/walks/handlers/get_walks_by_filters.rs
+++ api/src/features/walks/handlers/get_walks_by_filters.rs
@@ -1,9 +1,9 @@
 use axum::extract;
 
 use crate::errors;
-use crate::features::walks::dto;
 use crate::features::walks::models;
 use crate::features::walks::repositories;
+use crate::features::walks::{dto, requests};
 use crate::shared;
 use crate::state;
 
@@ -11,16 +11,22 @@ use crate::state;
     get,
     path = "/api/walks",
     tag = "Walks",
+    params(requests::GetWalksQuery),
     responses(
         (status = 200, description = "Returns a list of walks", body = Vec<dto::WalkDto>),
         (status = 500, description = "Internal Server Error", body = shared::ProblemDetails)
     )
 )]
-pub async fn get_all_walks(
+pub async fn get_walks_by_filters(
     extract::State(s): extract::State<state::AppState>,
+    extract::Query(query): extract::Query<requests::GetWalksQuery>,
 ) -> Result<axum::Json<Vec<dto::WalkDto>>, errors::ApiError> {
+    // validate query parameter
+    let command = query.validate_into(&s.pool).await?;
+
     let mut conn = s.pool.acquire().await?;
-    let rows = repositories::get_all_walks(conn.as_mut()).await?;
+
+    let rows = repositories::get_walks_by_filters(conn.as_mut(), &command).await?;
 
     let walks = rows
         .into_iter()
